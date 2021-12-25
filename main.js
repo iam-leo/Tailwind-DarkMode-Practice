2664b9256866e1ce109e2d9eb6039ca04dd5a9cb
Leonel Sosa <leo.vm.cba@gmail.com>
2021-12-25 18:18:45 -0300

    Added saving of the site theme in local storage

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 const btnSwitch = document.querySelector('#btnSwitch');
 const html = document.querySelector('html');
-console.log(html);
+let theme = localStorage.getItem('theme');
 
+//Recuperar dark mode si fue el ultimo utilizado
+if(theme && theme === 'dark'){
+    btnSwitch.classList.remove('after:left-0');
+    btnSwitch.classList.add('dark:after:right-0');
+    html.classList.add(theme);
+}
+
+//Button switch
 btnSwitch.addEventListener('click', function(){
     html.classList.toggle('dark')
     if(html.classList[0] === 'dark'){
         btnSwitch.classList.remove('after:left-0');
         btnSwitch.classList.add('dark:after:right-0');
+        localStorage.setItem('theme', 'dark');
     }else{
         btnSwitch.classList.add('after:left-0');
         btnSwitch.classList.remove('dark:after:right-0');
+        localStorage.setItem('theme', 'light');
     }
-})
+});
